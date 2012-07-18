ef7b8ce62b975665a4c467613ff3a2d660a3713d
pixeloution <erik@pixeloution.com>
2012-07-18 18:05:33 -0500

    allow empty data sets
    previously exited with error when provided with an empty array for obj.data -- ( 'obj.data should be array of arrays') - now allows either array of arrays, or an empty array to allow for empty data sets.

--- src/tablebuilder.js
+++ src/tablebuilder.js
@@ -161,9 +161,10 @@
     if( !Util._is_object(obj) || !obj.hasOwnProperty('data') )
       Util._exit('invalid data format - object should have a "data" property');
 
-    if(!(obj.data instanceof Array) || !(obj.data[0] instanceof Array))
-      Util._exit('invalid data format - obj.data expected to be array of arrays');
-
+    if(!(obj.data instanceof Array) || 
+      (!(obj.data[0] instanceof Array) && obj.data.length !== 0))    
+      Util._exit('invalid format - obj.data expected to be empty, or an array of arrays.');
+    
     return obj.data;
   }
 
